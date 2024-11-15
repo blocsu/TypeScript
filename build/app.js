ac977e7bf72acc2662fc2b0005624e8142fbe746
blocsu <dotsenko.sar@gmail.com>
2024-11-15 19:32:06 +0400

    Template Method

--- build/app.js
+++ build/app.js
@@ -2107,70 +2107,108 @@
 // console.log(auth.authUser(user));
 // auth.setStrategy(new GithubStrategy());
 // console.log(auth.authUser(user));
-//============== 14_117 Iterator ========================
-class Task {
-    constructor(priority) {
-        this.priority = priority;
+// //============== 14_117 Iterator ========================
+// class Task {
+// 	constructor(public priority: number) {}
+// }
+// class TaskList {
+// 	private tasks: Task[] = [];
+// 	public sortByPriority() {
+// 		this.tasks = this.tasks.sort((a, b) => {
+// 			if (a.priority > b.priority) {
+// 				return 1;
+// 			} else if (a.priority == b.priority) {
+// 				return 0;
+// 			}else {
+// 				return -1;
+// 			}
+// 		})
+// 	}
+// 	public addTask(task: Task) {
+// 		this.tasks.push(task);
+// 	}
+// 	public getTask() {
+// 		return this.tasks;
+// 	}
+// 	public count() {
+// 		return this.tasks.length;
+// 	}
+// 	public getIterator() {
+// 		return new PriorityTaskIterator(this);
+// 	}
+// }
+// interface IIterator<T> {
+// 	current(): T | undefined;
+// 	next(): T | undefined;
+// 	prev(): T | undefined;
+// 	index(): number;
+// }
+// class PriorityTaskIterator implements IIterator<Task> {
+// 	private position: number = 0;
+// 	private taskList: TaskList;
+// 	constructor(taskList: TaskList) {
+// 		taskList.sortByPriority();
+// 		this.taskList = taskList;		
+// 	}
+// 	current(): Task | undefined {
+// 		return this.taskList.getTask()[this.position];
+// 	}
+// 	next(): Task | undefined {
+// 		this.position += 1;
+// 		return this.taskList.getTask()[this.position];
+// 	}
+// 	prev(): Task | undefined {
+// 		this.position -= 1;
+// 		return this.taskList.getTask()[this.position];
+// 	}
+// 	index(): number {
+// 		return this.position;
+// 	}
+// }
+// const taskList = new TaskList();
+// taskList.addTask(new Task(8));
+// taskList.addTask(new Task(1));
+// taskList.addTask(new Task(3));
+// const iterator = taskList.getIterator();
+// console.log(iterator.current());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.prev());
+// console.log(iterator.index());
+//============== 14_118 Template Method ========================
+class Form {
+    constructor(name) {
+        this.name = name;
+    }
+    ;
+}
+class SaveForm {
+    save(form) {
+        const res = this.fill(form);
+        this.log(res);
+        this.send(res);
+    }
+    log(data) {
+        console.log(data);
     }
 }
-class TaskList {
-    constructor() {
-        this.tasks = [];
+class FirstAPI extends SaveForm {
+    fill(form) {
+        return form.name;
     }
-    sortByPriority() {
-        this.tasks = this.tasks.sort((a, b) => {
-            if (a.priority > b.priority) {
-                return 1;
-            }
-            else if (a.priority == b.priority) {
-                return 0;
-            }
-            else {
-                return -1;
-            }
-        });
-    }
-    addTask(task) {
-        this.tasks.push(task);
-    }
-    getTask() {
-        return this.tasks;
-    }
-    count() {
-        return this.tasks.length;
-    }
-    getIterator() {
-        return new PriorityTaskIterator(this);
+    send(data) {
+        console.log(`Отправляю ${data}`);
     }
 }
-class PriorityTaskIterator {
-    constructor(taskList) {
-        this.position = 0;
-        taskList.sortByPriority();
-        this.taskList = taskList;
+class SecondAPI extends SaveForm {
+    fill(form) {
+        return { fio: form.name };
     }
-    current() {
-        return this.taskList.getTask()[this.position];
-    }
-    next() {
-        this.position += 1;
-        return this.taskList.getTask()[this.position];
-    }
-    prev() {
-        this.position -= 1;
-        return this.taskList.getTask()[this.position];
-    }
-    index() {
-        return this.position;
+    send(data) {
+        console.log(`Отправляю ${data}`);
     }
 }
-const taskList = new TaskList();
-taskList.addTask(new Task(8));
-taskList.addTask(new Task(1));
-taskList.addTask(new Task(3));
-const iterator = taskList.getIterator();
-console.log(iterator.current());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.prev());
-console.log(iterator.index());
+const form1 = new FirstAPI();
+form1.save(new Form('Vasya'));
+const form2 = new SecondAPI();
+form2.save(new Form('Vasya'));
